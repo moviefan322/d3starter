3ca3fd913192b63e0b6238a27c15bc08a2226155
moviefan322 <philman202@yahoo.com>
2023-09-19 12:31:15 -0400

    intro to scales

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+const svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
+
 d3.json("data/buildings.json").then((data) => {
   data.forEach((d) => {
     d.height = Number(d.height);
   });
   console.log(data);
 
-  const svg = d3
-    .select("#chart-area")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+  const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
 
   const rectangles = svg.selectAll("rect").data(data);
 
@@ -20,6 +22,6 @@ d3.json("data/buildings.json").then((data) => {
     })
     .attr("y", 50)
     .attr("width", 25)
-    .attr("height", (d) => d.height)
+    .attr("height", (d) => y(d.height))
     .attr("fill", (d, i) => "green");
 });
