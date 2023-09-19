93ad5d6af10c921a0c928b6feac05885985d9cce
moviefan322 <philman202@yahoo.com>
2023-09-19 13:28:56 -0400

    axis generators

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
+
 const svg = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", 500)
-  .attr("height", 500);
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", WIDTH + MARGIN.TOP + MARGIN.BOTTOM);
+
+  const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+// X label
+g.append("text")
+  .attr("class", "x axis-label")
+  .attr("x", WIDTH / 2)
+  .attr("y", HEIGHT + 170)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("The word's tallest buildings");
+
+  // Y label
+  g.append("text")
+  .attr("class", "y axis-label")
+  .attr("x", -(HEIGHT / 2))
+  .attr("y", -60)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Height (m)");
 
 d3.json("data/buildings.json").then((data) => {
   data.forEach((d) => {
@@ -10,40 +37,65 @@ d3.json("data/buildings.json").then((data) => {
   });
   console.log(data);
 
+  const min = d3.min(data, (d) => d.height);
+  const max = d3.max(data, (d) => d.height);
+  const extent = d3.extent(data, (d) => d.height);
+  const names = data.map((d) => d.name);
+
   // band scale
   const x = d3
     .scaleBand()
-    .domain([
-      "Burj Khalifa",
-      "Shanghai Tower",
-      "Abraj Al-Bait Clock Tower",
-      "Ping An Finance Centre",
-      "Lotte World Tower",
-      "One World Trade Center",
-      "Guangzhou CTF Finance Centre",
-    ])
-    .range([0, 400])
+    .domain(names)
+    .range([0, WIDTH])
     .paddingInner(0.3)
     .paddingOuter(0.2);
 
-  console.log(x("Burj Khalifa"));
+  const y = d3.scaleLinear().domain([0, max]).range([0, HEIGHT]);
 
-  const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
+  const xAxisCall = d3.axisBottom(x);
+  g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${HEIGHT})`)
+    .call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
 
-  const rectangles = svg.selectAll("rect").data(data);
+  const yAxisCall = d3
+    .axisLeft(y)
+    .ticks(8)
+    .tickFormat((d) => d + "m");
+  g.append("g").attr("class", "y axis").call(yAxisCall);
+
+  const rectangles = g.selectAll("rect").data(data);
+
+  const color = d3
+    .scaleOrdinal()
+    .domain(names)
+    .range([
+      "#c11d1d",
+      "#eae600",
+      "#7ebd01",
+      "#f68a05",
+      "#f35e9a",
+      "#481e5d",
+      "#b10d6a",
+    ]);
 
   rectangles
     .enter()
     .append("rect")
     .attr("x", (d) => x(d.name))
-    .attr("y", 50)
+    .attr("y", 0)
     .attr("width", x.bandwidth)
     .attr("height", (d) => y(d.height))
-    .attr("fill", (d, i) => "green");
+    .attr("fill", (d, i) => color(d.name));
 });
 
 // log scale example:
-const log = d3.scaleLog().domain([300, 150000]).range([0, 400]).base(10);
+const log = d3.scaleLog().domain([300, 150000]).range([0, HEIGHT]).base(10);
 
 // time scale example:
 const time = d3
@@ -51,20 +103,7 @@ const time = d3
   .domain([new Date(2000, 0, 1), new Date(2001, 0, 1)])
   .range([0, 400]);
 
-const color = d3
-  .scaleOrdinal()
-  .domain(["apple", "lemon", "lime", "orange", "peach", "plum", "strawberry"])
-  .range([
-    "#c11d1d",
-    "#eae600",
-    "#7ebd01",
-    "#f68a05",
-    "#f35e9a",
-    "#481e5d",
-    "#b10d6a",
-  ]);
-
-const color2 = d3
-  .scaleOrdinal()
-  .domain(["africa", "europe", "asia", "americas", "oceania"])
-  .range(d3.schemaCategory10);
+// const color2 = d3
+//   .scaleOrdinal()
+//   .domain(["africa", "europe", "asia", "americas", "oceania"])
+//   .range(d3.schemaCategory10);
