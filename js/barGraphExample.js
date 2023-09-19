3e5269cec35df2c074c4066d7b83e1a53656cedc
moviefan322 <philman202@yahoo.com>
2023-09-19 14:30:56 -0400

    adds another bar graph with different data set

--- js/barGraphExample.js
+++ js/barGraphExample.js
@@ -8,7 +8,7 @@ const svg = d3
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", WIDTH + MARGIN.TOP + MARGIN.BOTTOM);
 
-const g = svg
+  const g = svg
   .append("g")
   .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
@@ -16,39 +16,40 @@ const g = svg
 g.append("text")
   .attr("class", "x axis-label")
   .attr("x", WIDTH / 2)
-  .attr("y", HEIGHT + 75)
+  .attr("y", HEIGHT + 170)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
-  .text("Month");
+  .text("The word's tallest buildings");
 
-// Y label
-g.append("text")
+  // Y label
+  g.append("text")
   .attr("class", "y axis-label")
   .attr("x", -(HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Revenue ($)");
 
-d3.csv("data/revenues.csv").then((data) => {
+d3.json("data/buildings.json").then((data) => {
   data.forEach((d) => {
-    d.revenue = Number(d.revenue);
-    d.profit = Number(d.profit);
+    d.height = Number(d.height);
   });
   console.log(data);
 
+  const min = d3.min(data, (d) => d.height);
+  const max = d3.max(data, (d) => d.height);
+  const extent = d3.extent(data, (d) => d.height);
+  const names = data.map((d) => d.name);
+
+  // band scale
   const x = d3
     .scaleBand()
-    .domain(data.map((d) => d.month))
+    .domain(names)
     .range([0, WIDTH])
     .paddingInner(0.3)
     .paddingOuter(0.2);
 
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.revenue)])
-    .range([HEIGHT, 0]);
+  const y = d3.scaleLinear().domain([0, max]).range([HEIGHT, 0]);
 
   const xAxisCall = d3.axisBottom(x);
   g.append("g")
@@ -67,9 +68,11 @@ d3.csv("data/revenues.csv").then((data) => {
     .tickFormat((d) => d + "m");
   g.append("g").attr("class", "y axis").call(yAxisCall);
 
+  const rectangles = g.selectAll("rect").data(data);
+
   const color = d3
     .scaleOrdinal()
-    .domain((d) => d.map((d) => d.month))
+    .domain(names)
     .range([
       "#c11d1d",
       "#eae600",
@@ -80,14 +83,26 @@ d3.csv("data/revenues.csv").then((data) => {
       "#b10d6a",
     ]);
 
-  const bars = g.selectAll("rect").data(data);
-
-  bars
+  rectangles
     .enter()
     .append("rect")
-    .attr("x", (d) => x(d.month))
-    .attr("y", (d) => y(d.revenue))
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.height))
     .attr("width", x.bandwidth)
-    .attr("height", (d) => HEIGHT - y(d.revenue))
-    .attr("fill", (d) => color(d.month));
+    .attr("height", (d) => HEIGHT - y(d.height))
+    .attr("fill", (d, i) => color(d.name));
 });
+
+// log scale example:
+const log = d3.scaleLog().domain([300, 150000]).range([0, HEIGHT]).base(10);
+
+// time scale example:
+const time = d3
+  .scaleTime()
+  .domain([new Date(2000, 0, 1), new Date(2001, 0, 1)])
+  .range([0, 400]);
+
+// const color2 = d3
+//   .scaleOrdinal()
+//   .domain(["africa", "europe", "asia", "americas", "oceania"])
+//   .range(d3.schemaCategory10);
